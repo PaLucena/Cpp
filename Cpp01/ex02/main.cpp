0d46f514d4b76c56ca6758b541bb69f04ff469ea
palucena <palucena@student.42malaga.com>
2023-12-26 16:55:52 +0100

    ex02 finished

---
+++ Cpp01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/26 16:35:06 by palucena          #+#    #+#             */
+/*   Updated: 2023/12/26 16:46:53 by palucena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string str ="HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << std::endl << "Memory addresses:" << std::endl;
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << std::endl << "Values:" << std::endl;
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
