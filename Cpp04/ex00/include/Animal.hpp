3b398c4a30500e2af03f4ca72fa7d526eb6d39a3
palucena <palucena@student.42malaga.com>
2024-02-13 18:25:42 +0100

    Started ex00

---
+++ Cpp04/ex00/include/Animal.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 17:42:50 by palucena          #+#    #+#             */
+/*   Updated: 2024/02/13 17:45:27 by palucena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <string>
+
+class	Animal {
+	protected:
+		std::string	type;
+} ;
+
+#endif
