4785d4396199cfe0264e4f6ecb3980e5e66f52f3
Pablo Lucena González <pablolucena01@gmail.com>
2024-03-10 21:13:17 +0100

    feat: ex02 done

--- Cpp07/ex02/include/Array.hpp
+++ Cpp07/ex02/include/Array.hpp
@@ -6,54 +6,73 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 16:13:53 by palucena          #+#    #+#             */
-/*   Updated: 2024/03/09 16:58:21 by palucena         ###   ########.fr       */
+/*   Updated: 2024/03/10 21:11:49 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+# include <iostream>
+# include <string>
+
 template <typename T>
 class Array {
 	private:
-		T	*_data;
-		int	_len;
+		T				*_data;
+		unsigned int	_len;
 	public:
 		Array();
 		Array(unsigned int n);
 		Array(const Array &toCopy);
-		Array	&operator=(const Array &toCopy)
+		Array	&operator=(const Array &toCopy);
 		~Array();
+
+		T	&operator[](unsigned int index);
+		unsigned int	size() const;
 };
 
 template <typename T>
 Array<T>::Array() : _data(NULL), _len(0) {}
 
 template <typename T>
-Array<T>::Array(unsigned int n) : _len(n) {
-	for (int i = 0; i < n; i++;)
-		_data[i] = new T;
-}
+Array<T>::Array(unsigned int n) : _data(new T[n]), _len(n) {}
 
 template <typename T>
-Array<T>::Array(const Array &toCopy) : _len(toCopy._len) {
-	for (int i = 0; i < n; i++;) {
-		_data[i] = new T;
+Array<T>::Array(const Array &toCopy) : _data(new T[toCopy._len]), _len(toCopy._len) {
+	for (unsigned int i = 0; i < toCopy._len; ++i)
 		_data[i] = toCopy._data[i];
-	}
 }
 
 template <typename T>
 Array<T>	&Array<T>::operator=(const Array &toCopy) {
-	if (this != &other) {
+	if (this != &toCopy) {
 		if (_data)
 			delete[] _data;
-		_data = new T[toCopy._len];
 		_len = toCopy._len;
-		for (int i = 0; i < _len; i++;)
+		_data = new T[_len];
+		for (unsigned int i = 0; i < _len; i++)
 			_data[i] = toCopy._data[i];
 	}
 	return *this;
+}
+
+template <typename T>
+Array<T>::~Array() {
+	if (_data)
+		delete[] _data;
+}
+
+template <typename T>
+T	&Array<T>::operator[](unsigned int index) {
+	if (index >= _len)
+		throw (std::exception());
+	return (_data[index]);
+}
+
+template <typename T>
+unsigned int	Array<T>::size() const {
+	return (_len);
 }
 
 #endif
--- Cpp07/ex02/src/main.cpp
+++ Cpp07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 16:13:22 by palucena          #+#    #+#             */
-/*   Updated: 2024/03/09 16:13:51 by palucena         ###   ########.fr       */
+/*   Updated: 2024/03/10 21:12:43 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,30 @@
 
 int	main(void)
 {
+	try {
+		Array<int> emptyArray;
+		std::cout << "Size of emptyArray: " << emptyArray.size() << std::endl;
+
+		Array<int> intArray(5);
+		std::cout << "Size of intArray: " << intArray.size() << std::endl;
+
+		intArray[0] = 33;
+		Array<int> copiedArray(intArray);
+	
+		intArray[0] = 66;
+		Array<int> assignedArray = intArray;
+
+		intArray[0] = 99;
+
+		std::cout << "First element of intArray: " << intArray[0] << std::endl;
+		std::cout << "First element of copiedArray: " << copiedArray[0] << std::endl;
+		std::cout << "First element of assignedArray: " << assignedArray[0] << std::endl;
+
+		std::cout << "Trying to access an out-of-range element..." << std::endl;
+		std::cout << intArray[10] << std::endl;
+
+	} catch (const std::exception &e) {
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+	}
 	return 0;
 }
