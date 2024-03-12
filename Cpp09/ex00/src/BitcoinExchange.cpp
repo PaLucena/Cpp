f73cae6ee77618b6fef25ab2d76d09a354aa00a8
Pablo Lucena González <pablolucena01@gmail.com>
2024-03-12 21:34:53 +0100

    feat: :zap: ex00

--- Cpp09/ex00/src/BitcoinExchange.cpp
+++ Cpp09/ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 14:24:37 by palucena          #+#    #+#             */
-/*   Updated: 2024/03/12 18:20:00 by palucena         ###   ########.fr       */
+/*   Updated: 2024/03/12 21:31:49 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ BitcoinExchange::BitcoinExchange(const BitcoinExchange &toCopy): _db(toCopy._db)
 BitcoinExchange::BitcoinExchange(const std::string &fileName) {
 	std::ifstream	file;
 	std::string		line;
-	int				colon;
+	size_t			colon;
 
 	file.open("../" + fileName);
 	if (!file.is_open())
@@ -29,10 +29,11 @@ BitcoinExchange::BitcoinExchange(const std::string &fileName) {
 		while (!line.empty()) {
 			std::getline(file, line);
 			colon = line.find(',');
-			if (colon == line.size() - 1)
+			if (colon == std::string::npos)
 				throw (BitcoinExchange::BadInputError());
 			_db.insert({line.substr(0, colon), atof(line.substr(colon + 1, line.length()).c_str())});
 		}
+		file.close();
 	}
 }
 
@@ -45,5 +46,22 @@ BitcoinExchange	&BitcoinExchange::operator=(const BitcoinExchange &toCopy) {
 }
 
 void	BitcoinExchange::compareDataBases(const std::string &fileName) {
+	std::ifstream	file;
+	std::string		line;
+	size_t			pipe;
 	
+	file.open("../" + fileName);
+	if (!file.is_open())
+		throw (BitcoinExchange::BadInputError());
+	else {
+		std::getline(file, line);
+		while (!line.empty()) {
+			std::getline(file, line);
+			pipe = line.find('|');
+			if (pipe == std::string::npos)
+				throw (BitcoinExchange::BadInputError());
+			
+		}
+		file.close();
+	}
 }
